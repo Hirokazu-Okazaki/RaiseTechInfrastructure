f0da5261e28d4e09bfc7f0697c3ab404dc621c5d
Hirokazu-Okazaki <gm.okazaki@gmail.com>
2020-02-18 13:40:52 +0000

    ServerSpecのテストを追記

--- serverspec/spec/aws/sample_spec.rb
+++ serverspec/spec/aws/sample_spec.rb
@@ -1,28 +1,69 @@
 require 'spec_helper'
 
-describe package('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_installed }
+# Amazon Linux2 のためamazonを指定
+current_os_family = "amazon"
+
+# コンテナ名を指定(docker-composeで起動するデフォルトの名前を指定)
+container_names = [
+  'raisetechmysqlapp_app_1',
+  'raisetechmysqlapp_web_1'
+]
+
+# ドメイン名
+domain_name = "www.raisetechportfolio.tk"
+
+describe 'docker' do
+  # dockerが使用できること
+  describe service('docker'), :if => os[:family] == current_os_family do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  # docker-composeが使用できること
+  describe command('docker-compose help') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  # コンテナが起動していること
+  container_names.each { |container_name|
+    describe docker_container(container_name) do
+      it { should exist }
+      it { should be_running }
+    end
+  }
 end
 
-describe package('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_installed }
+describe host(domain_name) do
+  # ドメイン名の名前解決
+  it { should be_resolvable }
+  it { should be_reachable }
 end
 
-describe service('httpd'), :if => os[:family] == 'redhat' do
-  it { should be_enabled }
-  it { should be_running }
+describe command("curl -L https://#{domain_name} -o /dev/null -w '%{http_code}\n' -s") do
+  # http_codeだけ抜き出してテスト 200:正常
+  its(:stdout) { should match '200' }
 end
 
-describe service('apache2'), :if => os[:family] == 'ubuntu' do
-  it { should be_enabled }
-  it { should be_running }
+describe 'listening port' do
+  describe 'HTTP' do
+    describe port(80) do
+      it { should be_listening }
+    end
+  end
+
+  describe 'SSH' do
+    describe port(22) do
+      it { should be_listening }
+    end
+  end
 end
 
-describe service('org.apache.httpd'), :if => os[:family] == 'darwin' do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe port(80) do
-  it { should be_listening }
+describe 'not listening port' do
+  describe 'HTTPS' do
+    # HTTPSで通信するのはロードバランサーまで
+    # ロードバランサーからEC2への経路はHTTPで通信
+    describe port(443) do
+      it { should_not be_listening }
+    end
+  end
 end
